590d2d29fba5d75d159bdbd62e4e97133cc3d694
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2021-05-16 16:53:05 +0300

    refactor: add TypeScript

--- src/redux/app-reducer.tsx
+++ src/redux/app-reducer.tsx
@@ -2,20 +2,28 @@ import {setUser} from "./auth-reducer";
 
 const SET_AUTHORIZATION = "social-network/app/SET_AUTHORIZATION";
 
-const setAuthorization = () => ({
+type SETAUTORIZATIONTYPE = () => ({
+  type: typeof SET_AUTHORIZATION
+})
+
+const setAuthorization: SETAUTORIZATIONTYPE = () => ({
   type: SET_AUTHORIZATION
 });
 
 const initialState = {
-  isInitialized: false
+  isInitialized: false as boolean
 }
 
-export const initializeApp = () => async (dispatch) => {
+export const initializeApp = () => async (dispatch: Function) => {
   await dispatch(setUser())
   await dispatch(setAuthorization())
 }
 
-export const appReducer = (state = initialState, action) => {
+type ACTIONTYPE = {
+  type: string
+}
+
+export const appReducer = (state = initialState, action: ACTIONTYPE) => {
   switch (action.type) {
     case SET_AUTHORIZATION: {
       return {
