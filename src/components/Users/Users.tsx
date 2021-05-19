c084eb41351e44e2fca58a80614c706e495e3919
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2021-05-19 21:35:32 +0300

    refactor: typed Users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import {Paginator} from "../Common/Paginator/Paginator";
 import {User} from "./User";
+import {UserType} from "../../Types/types";
 
-const Users = (props) => {
+type PropsType = {
+    totalUsersCount: number
+    usersPerPage: number
+    currentPage: number
+    onPageChanged: (pageNumber: number) => void
+    countOfDisplayingPages: number
+    users: Array<UserType>
+    follow: () => void
+    unFollow: () => void
+    usersWithToggleFollowing: Array<number>
+}
+
+const Users: React.FC<PropsType> = (props) => {
   return (
       <div>
         <Paginator totalUsersCount={props.totalUsersCount}
